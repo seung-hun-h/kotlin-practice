8da1606a6a404ca10f202d365cb4d8986bdd3902
seunghun <dau4723@gmail.com>
2023-05-25 23:52:28 +0900

    Refactor 불필요한 코드 제거

--- readBooks/src/main/java/com/example/readbooks/shell/BookCommand.kt
+++ readBooks/src/main/java/com/example/readbooks/shell/BookCommand.kt
@@ -4,8 +4,6 @@ import com.example.readbooks.book.Book
 import com.example.readbooks.book.BookManager
 import org.springframework.shell.component.flow.ComponentFlow
 import org.springframework.shell.component.flow.ResultMode
-import org.springframework.shell.component.flow.SelectItem
-import org.springframework.shell.standard.AbstractShellComponent
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 import org.springframework.shell.standard.ShellOption
@@ -15,7 +13,7 @@ import org.springframework.shell.standard.ShellOption
 open class BookCommand(
 	private val bookService: BookManager,
 	private val componentFlowBuilder: ComponentFlow.Builder
-): AbstractShellComponent() {
+) {
 	@ShellMethod(value = "List books", key = ["books list"])
 	fun findAllBooks(): String {
 		return convert(bookService.findAll())
@@ -80,68 +78,5 @@ open class BookCommand(
 		bookService.save(book)
 
 		return "Book is saved!!"
-	}
-
-	@ShellMethod(key = ["flow showcase2"], value = "Showcase with options", group = "Flow")
-	open fun showcase2(
-		@ShellOption(help = "Field1 value", defaultValue = ShellOption.NULL) field1: String?,
-		@ShellOption(help = "Field2 value", defaultValue = ShellOption.NULL) field2: String?,
-		@ShellOption(help = "Confirmation1 value", defaultValue = ShellOption.NULL) confirmation1: Boolean?,
-		@ShellOption(help = "Path1 value", defaultValue = ShellOption.NULL) path1: String?,
-		@ShellOption(help = "Single1 value", defaultValue = ShellOption.NULL) single1: String?,
-		@ShellOption(help = "Multi1 value", defaultValue = ShellOption.NULL) multi1: List<String>?
-	): String? {
-		val single1SelectItems: MutableMap<String, String> = HashMap()
-		single1SelectItems["key1"] = "value1"
-		single1SelectItems["key2"] = "value2"
-		val multi1SelectItems: List<SelectItem> = listOf(
-			SelectItem.of("key1", "value1"),
-			SelectItem.of("key2", "value2"), SelectItem.of("key3", "value3")
-		)
-		val multi1ResultValues = multi1 ?: ArrayList()
-		val flow = componentFlowBuilder.clone().reset()
-			.withStringInput("field1")
-			.name("Field1")
-			.defaultValue("defaultField1Value")
-			.resultValue(field1)
-			.resultMode(ResultMode.ACCEPT)
-			.and()
-			.withStringInput("field2")
-			.name("Field2")
-			.resultValue(field2)
-			.resultMode(ResultMode.ACCEPT)
-			.and()
-			.withConfirmationInput("confirmation1")
-			.name("Confirmation1")
-			.resultValue(confirmation1)
-			.resultMode(ResultMode.ACCEPT)
-			.and()
-			.withPathInput("path1")
-			.name("Path1")
-			.resultValue(path1)
-			.resultMode(ResultMode.ACCEPT)
-			.and()
-			.withSingleItemSelector("single1")
-			.name("Single1")
-			.selectItems(single1SelectItems)
-			.resultValue(single1)
-			.resultMode(ResultMode.ACCEPT)
-			.and()
-			.withMultiItemSelector("multi1")
-			.name("Multi1")
-			.selectItems(multi1SelectItems)
-			.resultValues(multi1ResultValues)
-			.resultMode(ResultMode.ACCEPT)
-			.and()
-			.build()
-		val result = flow.run()
-		val buf = StringBuilder()
-		result.context.stream().forEach { (key, value): Map.Entry<Any?, Any?> ->
-			buf.append(key)
-			buf.append(" = ")
-			buf.append(value)
-			buf.append("\n")
-		}
-		return buf.toString()
 	}
 }
