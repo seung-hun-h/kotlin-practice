2c1b295a733e40fb6aa058de928bec3f07f24907
seunghun <dau4723@gmail.com>
2023-06-07 23:07:31 +0900

    Refactor 예약 클래스 이름 변경

--- readBooks/src/main/java/com/example/readbooks/reservation/repository/ReservationRepository.kt
+++ readBooks/src/main/java/com/example/readbooks/reservation/repository/ReservationRepository.kt
@@ -5,11 +5,12 @@ import com.example.readbooks.reservation.entity.ReservationStatus
 import org.springframework.data.repository.CrudRepository
 import java.time.LocalDate
 
-interface BookReservationRepository: CrudRepository<BookReservation, Long>{
+interface ReservationRepository: CrudRepository<BookReservation, Long>{
 	fun existsByBookIdAndReservist(bookId: Long, reservist: String): Boolean
 
 	fun existsByBookIdAndStartAtAndReservationStatusIsIn(bookId: Long, startAt: LocalDate, reservationStatus: List<ReservationStatus>): Boolean
 
 	fun findByBookId(bookId: Long): List<BookReservation>
+
 	fun findByBookIdAndReservistAndStartAtAndReservationStatus(bookId: Long, reservist: String, startAt: LocalDate, reserved: ReservationStatus): BookReservation?
 }
